8a5ff1f297239d90a5b97cdd36c911501aa1bbb2
Manuli1998 <manuli1998@gmail.com>
2020-11-08 10:12:59 +0530

    Registration issue

---
+++ views/register/regForm-header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Volunteer Registration</title>
+  <link rel="stylesheet" href="<?= URL ?>public/css/vol-register-page-style.css" />
+
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet" />
+  <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+  <style>
+    body {
+
+      background-color: black;
+
+    }
+  </style>
+</head>
+
+<body>
+
+  <header>
+    <div class="nav-bar">
+      <div class="logo-container">
+
+
+        <div class="logo"></div>
+      </div>
+
+      <div class="title-name">
+
+        <h3 class="logo-name">
+          <div class="tzu-logo"></div>
+          Tzu Chi Foundation | <span> Hambantota</span>
+        </h3>
+      </div>
+
+      <div class="log-sign" style="--i: 1.8s;">
+
+        <a href="<?= URL ?>index" class="back-btn" alt="Sun"><i class="fas fa-home"></i></a>
+      </div>
+
+    </div>
+
+  </header>
--- views/register/volRegForm.php
+++ views/register/volRegForm.php
@@ -1,50 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Volunteer Registration</title>
-  <link rel="stylesheet" href="<?= URL ?>public/css/vol-register-page-style.css" />
-
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet" />
-  <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
-  <style>
-    body {
-
-      background-color: black;
-
-    }
-  </style>
-</head>
-
-<body>
-
-  <header>
-    <div class="nav-bar">
-      <div class="logo-container">
-
-
-        <div class="logo"></div>
-      </div>
-
-      <div class="title-name">
-
-        <h3 class="logo-name">
-          <div class="tzu-logo"></div>
-          Tzu Chi Foundation | <span> Hambantota</span>
-        </h3>
-      </div>
-
-      <div class="log-sign" style="--i: 1.8s;">
-
-        <a href="<?= URL ?>index" class="back-btn" alt="Sun"><i class="fas fa-home"></i></a>
-      </div>
-
-    </div>
-
-  </header>
+  <?php include "regForm-header.php";?>
+  
+  
   <div class="login" style="background-image: url('<?= URL ?>public/images/volbg222.jpg') ;background-size:cover">
     <h1>Welcome to Volunteer Registration</h1>
     <div id="error"></div>
@@ -118,13 +75,6 @@
     </form>
   </div>
 
-  <footer>
-    <div class="footer">Powered by<h4>Humanity2020&copy;</h4>
-    </div>
-  </footer>
-  <script type="text/javascript" src="../../public/js/error.js"></script>
+  <?php include "regForm-footer.php";?>
 
 
-</body>
-
-</html>
