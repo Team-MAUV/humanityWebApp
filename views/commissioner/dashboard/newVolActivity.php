7d299e0395ca99bae301164117d2eceb4968fb3a
anjana-dodampe <ahmdodampe@hotmail.com>
2020-11-10 16:26:01 +0530

    Session In charge, Project reports UI's placed

--- views/commissioner/dashboard/newVolActivity.php
+++ views/commissioner/dashboard/newVolActivity.php
@@ -12,15 +12,15 @@ include "com-dash-header.php"; ?>
       <label for="id">ID</label>
       <label for="name">Activity Name</label>
       <input type="text" name="id" placeholder="26" value="auto" id="id">
-      <input type="text" name="name" placeholder="John Doe" id="name">
-      <label for="email">Email</label>
-      <label for="phone">Phone</label>
-      <input type="text" name="email" placeholder="johndoe@example.com" id="email">
-      <input type="text" name="phone" placeholder="2025550143" id="phone">
-      <label for="title">Title</label>
-      <label for="created">Created</label>
-      <input type="text" name="title" placeholder="Employee" id="title">
-      <input type="datetime-local" name="created" value="<?= date('Y-m-d\TH:i') ?>" id="created">
+      <input type="text" name="name" placeholder="Blood Donation" id="name">
+      <label for="venue">Venue</label>
+      <label for="p_count">Expected Participant Count</label>
+      <input type="text" name="venue" id="venue">
+      <input type="text" name="p_count" id="p_count">
+      <label for="start_time">Starting Date & Time</label>
+      <label for="end_time">Ending Date & Time</label>
+      <input type="datetime-local" name="created" value="<?= date('Y-m-d\TH:i') ?>" id="start_time">
+      <input type="datetime-local" name="created" value="<?= date('Y-m-d\TH:i') ?>" id="end_time">
       <input type="submit" value="Create">
     </form>
 
--- views/commissioner/dashboard/sessionIncharge.php
+++ views/commissioner/dashboard/sessionIncharge.php
@@ -6,11 +6,36 @@ include "com-dash-header.php"; ?>
 
 <div class="grid-container">
 
-  sessionIncharge
+  <div class="content update">
+    <h2>Appoint a Session Incharge</h2>
+    <form action="create.php" method="post">
+      <label for="venue">Select Staff ID</label>
+      <div class="select"> <select name="staff-id">
+          <option selected="selected">Choose one</option>
+          <?php
+          // A sample product array
+          $products = array("Mobile", "Laptop", "Tablet", "Camera");
+
+          // Iterating through the product array
+          foreach ($products as $item) {
+            echo '<option value="' . strtolower($item) . '">' . $item . '</option>';
+          }
+          ?>
+        </select></div>
+      <br>
+      <h3>Enter Session Incharge Credentials </h3>
+      <label for="tmp_username">Session Incharge Temporary Username</label>
+      <input type="text" name="tmp_username" id="tmp_username">
+      <label for="p_count">Expected Participant Count</label>
+      <input type="text" name="p_count" id="p_count">
+
+      <input type="submit" value="Appoint">
+
+    </form>
 
 
 
-</div>
+  </div>
 
 
-<?php include "com-dash-footer.php" ?>
+  <?php include "com-dash-footer.php" ?>
