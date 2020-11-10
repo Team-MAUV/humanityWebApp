1aed58db717de341dabc25af95d0b98e7e467262
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2020-11-10 19:11:21 +0530

    session home add

--- views/session-incharge/dashboard/sess_header.php
+++ views/session-incharge/dashboard/sess_header.php
@@ -33,7 +33,7 @@
             <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content">
-            <a href="#">Edit Profile</a>
+         
             <a href="<?= URL ?>Sessionincharge/logout">Logout</a>
 
           </div>
@@ -57,47 +57,23 @@
           <li class="<?php if ($page == 'home') {
                         echo 'active';
                       } ?>">
-            <a href="<?= URL ?>staff/index"><i class="fas fa-home"></i> Home</a>
+            <a href="<?= URL ?>sessionincharge/index"><i class="fas fa-home"></i> Home</a>
           </li>
 
-          <li class="<?php if ($page == 'Market_Module') {
-                        echo 'active';
-                      } ?>">
-            <a href="#" class="feat-btn"><i class="fa fa-shopping-cart" aria-hidden="true"></i> Market Module
-              <span class="fas fa-caret-down first"></span>
-            </a>
-            <ul class="feat-show">
-              <li class="<?php if ($page == 'add_product') {
-                            echo 'active';
-                          } ?>"><a href="<?= URL ?>staff/add_product">Add product  </a></li>
-              <li class="<?php if ($page == 'access_product') {
-                            echo 'active';
-                          } ?>"><a href="<?= URL ?>staff/access_product">Access product</a></li>
-              
-            </ul>
-          </li>
+         
           
-          <li class="<?php if ($page == 'staff_report') {
+          <li class="<?php if ($page == 'media_gallary') {
                         echo 'active';
                       } ?>">
-            <a href="<?= URL ?>staff/staff_report"><i class="fas fa-file-contract"></i> Reports</a>
+            <a href="<?= URL ?>sessionincharge/media_gallary"><i class="fas fa-file-contract"></i> Media</a>
           </li>
-          <li class="<?php if ($page == 'requestleave') {
+          <li class="<?php if ($page == 'attendance_marking') {
                         echo 'active';
                       } ?>">
-            <a href="<?= URL ?>staff/requestleave"><i class="fas fa-id-card-alt"></i> Request Leave</a>
+            <a href="<?= URL ?>sessionincharge/attendance_marking"><i class="fas fa-id-card-alt"></i> Attendance Marking</a>
           </li>
 
-          <li class="<?php if ($page == 'staff_gallery') {
-                        echo 'active';
-                      } ?>">
-            <a href="<?= URL ?>Staff/staff_gallery"><i class="fas fa-photo-video"> </i> Media</a>
-          </li>
-          <li class="<?php if ($page == 'staff_notification') {
-                        echo 'active';
-                      } ?>">
-            <a href="<?= URL ?>staff/staff_notification"><i class="fa fa-bell" aria-hidden="true"></i> Notification</a>
-                    </li>
+        
         
 
         </ul>
