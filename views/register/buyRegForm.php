6adc9b1a2eec06e7b66390834f34cebb02d15815
Manuli1998 <manuli1998@gmail.com>
2020-11-11 14:12:48 +0530

    Buyregform emptyfield validation

--- views/register/buyRegForm.php
+++ views/register/buyRegForm.php
@@ -85,89 +85,6 @@ include "regForm-header.php"; ?>
 
     </form>
   </div>
-<?php $title = 'Donor Registration';
-$style = 'register-donRegForm.css';
-include "regForm-header.php"; ?>
-
-   
-
-
-   
-    <div class="login" style="background-image: url('http://localhost/humanity/public/images/regbg22.jpg') ;">
-        <h1>Welcome to Donor Registration</h1>
-        <div id="error"></div>
-        <form action="run_don_register" method="POST" id="form2">
-
-            <label for="fullname">
-                <i class="fas fa-user"></i>
-            </label>
-            <input type="text" name="name" placeholder="Full Name" id="name" required>
-
-            <label for="nic">
-                <i class="fa fa-id-card "></i>
-            </label>
-            <input type="text" name="nic" placeholder="National Id Number" id="nic" required>
-
-            <label for="emailaddress">
-                <i class="fas fa-envelope"></i>
-            </label>
-            <input type="text" name="email" placeholder="Email Address" id="email" required>
-            <div class="validation-error">
-            </div>
-
-            <label for="permanentaddress">
-                <i class="fa fa-address-book  "></i>
-            </label>
-            <input type="text" name="address" placeholder="Permanent Address" id="address" required>
-            <div class="validation-error">
-            </div>
-
-            <label for="username">
-                <i class="fa fa-address-book  "></i>
-            </label>
-            <input type="text" name="username" placeholder="Username" id="username" required>
-            <div class="validation-error">
-            </div>
-
-            <label for="password">
-                <i class="fa fa-address-book  "></i>
-            </label>
-            <input type="password" name="password" placeholder="Password" id="password" required>
-            <div class="validation-error">
-            </div>
-
-            <label for="contact">
-                <i class="fa fa-phone-square "></i>
-            </label>
-            <input type="text" name="contact" placeholder="Contact Number" id="contact" required>
-            <div class="validation-error">
-            </div>
-
-
-            <label for="gender">
-                <div class="i " style="align-items: left; justify-content: flex-start; ">
-                    &nbsp<i class="fa fa-male fa-2x "> </i>
-                </div>
-            </label>
-            <select name="gender">
-
-                <option value="male ">Male</option>
-                <option value="female ">Female</option>
-                <option value="other ">Other</option>
-            </select>
-            <div class="validation-error ">
-            </div>
-
-            <button class="btn btnreg " type="submit " name="register " onclick="validation();">
-                <p style="font-weight: 800;"> Submit</p>
-            </button>
-
-
-
-
-
-        </form>
-    </div>
     <script>
 function validation() {
     
@@ -178,7 +95,16 @@ function validation() {
     
     var at = email.indexOf("@");
     var dot = email.lastIndexOf(".");
+    if((name==null ||name=="")||(nic==null ||nic=="")||(email==null ||email=="")||(address==null ||address=="")||(contact==null ||contact=="")||(username==null ||username=="")||([password]==null ||password=="")||(gender==null ||gender=="")){
+      swal({
+            title: "Registration Faied!",
+            text: "Please fill the required fields",
 
+            icon: "error",
+
+        });
+    }
+else{
     if ((contact.length != 10) || (isNaN(contact))) {
         swal({
           
@@ -210,7 +136,7 @@ function validation() {
 
         }
     }
-
+  }
 }
 </script>
     <?php include "regForm-footer.php"; ?>
