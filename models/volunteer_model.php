6b2dc03cfd1ae12d015ff9aaf3d3da1f1b9f8e3a
anjana-dodampe <ahmdodampe@hotmail.com>
2020-11-12 14:13:29 +0530

    Adding Beneficiary cases successfully completed!

--- models/volunteer_model.php
+++ models/volunteer_model.php
@@ -6,67 +6,77 @@ class volunteer_Model extends Model
   public function __construct()
   {
     parent::__construct();
-
   }
-  
+
 
   public function vol_profile()
   {
-     
-    $un= $_SESSION['name'];
-echo $un;
+
+    $un = $_SESSION['name'];
+    echo $un;
     $st = $this->db->prepare("SELECT `name`,`level`,`vol_points`,`prev_vol_exp` FROM `volunteer` WHERE `username`='$un' ");
     $st->execute();
-    $volprofile=$st->fetchAll();
+    $volprofile = $st->fetchAll();
     print_r($volprofile);
-    $voldata=[
-        'volprofile' => $volprofile
+    $voldata = [
+      'volprofile' => $volprofile
     ];
-    return($voldata);
-
-
+    return ($voldata);
   }
 
-public function addbeneficiery(){
+  public function addbeneficiery()
+  {
     $msg = '';
-    if (isset($_FILES['file'],$_POST['vol_id'])) {
+    if (isset($_FILES['file'], $_POST['vol_id'])) {
       $target_dir = $_SERVER['DOCUMENT_ROOT'] . '/humanity/public/beneficiery_case/';
       $save_path = 'beneficiery_case/';
 
       $dest_path = $target_dir . basename($_FILES['file']['name']);
       $file_path = $save_path . basename($_FILES['file']['name']);
-      
+
       if (!empty($_FILES['file']['tmp_name']) && filesize($_FILES['file']['tmp_name'])) {
         if (file_exists($dest_path)) {
           $msg = "This File already exists, please choose another or rename that file.";
         } else if ($_FILES['file']['size'] > 500000) {
           $msg = "File size is too large, please choose a file less than 500kb.";
-        }else {
-         
-            // Everything checks out now we can move the uploaded image
+        } else {
+
+          $get_volid = $this->db->prepare("SELECT id FROM volunteer WHERE vol_id= :volid  ");
+          $get_volid->execute(array(
+            ':volid' => $_POST['vol_id']
+          ));
+
+          $result = $get_volid->fetchAll();
+
+          if ($result > 0) {
+            foreach ($result as $tmp) :
+              $id = $tmp['id'];
+            endforeach;
+          }
+
+          $get_volid = $this->db->prepare('INSERT INTO beneficiery_case(vol_id,case_path) VALUES (:id,:path)');
+          $get_volid->execute(array(
+            ':id' => $id,
+            ':path' => $file_path
+          ));
+          $flag = $get_volid->execute();
+
+          if ($flag) {
             move_uploaded_file($_FILES['file']['tmp_name'], $dest_path);
-  
-            // Insert image info into the database (title, description, image path, and date added)
-           // Insert image info into the database (title, description, image path, and date added)
-          $stmt = $this->db->prepare('INSERT INTO beneficiery_case VALUES ( NULL,NULL,?,NULL,NULL,NULL, ?,NULL)');
-          $stmt->execute([$_POST['vol_id'], $file_path]);
-  
-          $msg = "File uploaded successfully!";
+            $msg = "File uploaded successfully!";
+          } else {
+            $msg = "File upload  Failed!";
+          }
         }
       } else {
         $msg = 'Please upload a file!';
       }
-
     }
-      $pageData = [
 
-        'msg' => $msg
-      ];
-      return $pageData;
-    
+    $pageData = [
+
+      'msg' => $msg
+    ];
+    return $pageData;
   }
-  }
-
-
-
-
+}
--- views/volunteer/dashboard/addNewBeneficiary.php
+++ views/volunteer/dashboard/addNewBeneficiary.php
@@ -2,11 +2,11 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Simple Layouts</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Simple Layouts</title>
 
-  <link rel="stylesheet" href="<?= URL ?>public/css/vol-addnewbene.css" />
+    <link rel="stylesheet" href="<?= URL ?>public/css/vol-addnewbene.css" />
 
 
 </head>
@@ -18,12 +18,12 @@ include "vol_dash_header.php"; ?>
 
 
 
-<h1>New Beneficiery Case file</h1>
+    <h1>New Beneficiery Case file</h1>
     <form action="addbeneficiery" method="POST" id="beneform" enctype="multipart/form-data">
-    <img src="<?=URL?>public/images/upload.svg" class="floatRight">
+        <img src="<?= URL ?>public/images/upload.svg" class="floatRight">
         <ul class="voldata">
-        
-            
+
+
             <li>
                 Volunteer ID: <input type="text" name="vol_id" id="vid" required>
             </li>
@@ -31,7 +31,8 @@ include "vol_dash_header.php"; ?>
                 <div class="formcss">
                     <p style="align-content: center;">Upload the file: </p>
                     <input type="file" id="beneficierycase" name="file" accept="file/*" class="file">
-                    <input type="submit" class="btn" value="Upload"></div>
+                    <input type="submit" class="btn" value="Upload">
+                </div>
             </li>
         </ul>
 
@@ -50,4 +51,5 @@ include "vol_dash_header.php"; ?>
 
 
 <?php include "vol_dash_footer.php" ?>
+
 </html>
