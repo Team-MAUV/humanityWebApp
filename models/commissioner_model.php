08097e10254f2b6583b87b5c37884b42ba1940d6
anjana-dodampe <ahmdodampe@hotmail.com>
2020-11-16 21:09:09 +0530

    Volunteer Requests updated

--- models/commissioner_model.php
+++ models/commissioner_model.php
@@ -12,24 +12,35 @@ class commissioner_Model extends Model
 
   public function get_reg_vol_profiles()
   {
-    $st = $this->db->prepare('SELECT * FROM volunteer ORDER BY id LIMIT :current_page, :record_per_page');
-    // Get the page via GET request (URL param: page), if non exists default the page to 1
-    $page_no = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
 
-    // Number of records to show on each page
-    $records_per_page = 5;
+        //Volunteer Profiles
+        $st = $this->db->prepare('SELECT * FROM volunteer ORDER BY id LIMIT :current_page, :record_per_page');
+        // Get the page via GET request (URL param: page), if non exists default the page to 1
+        $page_no = isset($_GET['page']) && is_numeric($_GET['page']) ? (int)$_GET['page'] : 1;
 
-    $st->bindValue(':current_page', ($page_no - 1) * $records_per_page, PDO::PARAM_INT);
-    $st->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
-    $st->execute();
-    $num_contacts = $this->db->query('SELECT COUNT(*) FROM volunteer')->fetchColumn();
-    $contacts = $st->fetchAll();
+        // Number of records to show on each page
+        $records_per_page = 5;
+
+        $st->bindValue(':current_page', ($page_no - 1) * $records_per_page, PDO::PARAM_INT);
+        $st->bindValue(':record_per_page', $records_per_page, PDO::PARAM_INT);
+        $st->execute();
+        $num_contacts = $this->db->query('SELECT COUNT(*) FROM volunteer')->fetchColumn();
+        $contacts = $st->fetchAll();
+
+
+
+        //Volunteer Requests
+        $stmt = $this->db->prepare('SELECT * FROM volunteer WHERE status=0 ORDER BY join_date ');        
+        $stmt->execute();
+        $newReq = $stmt->fetchAll();
+
     //All the data that has to be return from this functon is added to an associative array
     $pageData = [
       'page_no' => $page_no,
       'records_per_page' => $records_per_page,
       'contacts' => $contacts,
-      'num_contacts' => $num_contacts
+      'num_contacts' => $num_contacts,
+      'newReq'=> $newReq
     ];
     return ($pageData);
   }
--- views/commissioner/dashboard/volunteer.php
+++ views/commissioner/dashboard/volunteer.php
@@ -1,6 +1,7 @@
 <?php $page = 'home';
 $title = 'Volunteer Profiles';
 $style = 'com-dash-profiles-volunteer.css';
+$jsFile = 'tab-changing';
 include "com-dash-header.php"; ?>
 
 <div class="grid-container">
@@ -26,17 +27,17 @@ include "com-dash-header.php"; ?>
       </ul>
 
 
-
       <hr>
     </div>
 
 
     <div id="tab1C" class="tab-container">
-      <div class="content read">
 
-        <table>
-          <thead>
-            <tr>
+          <div class="content read">
+
+            <table>
+              <thead>
+              <tr>
               <td>#</td>
               <td>Volunteer ID</td>
               <td>Name</td>
@@ -45,46 +46,71 @@ include "com-dash-header.php"; ?>
               <td>Joined Date</td>
               <td></td>
             </tr>
+              </thead>
+              <tbody>
+
+                <?php foreach ($contacts as $contact) : ?>
+                  <tr>
+                    <td><?= $contact['id'] ?></td>
+                    <td><?= $contact['vol_id'] ?></td>
+                    <td><?= $contact['name'] ?></td>
+                    <td><?= $contact['level'] ?></td>
+                    <td><?= $contact['vol_points'] ?></td>
+                    <td><?= $contact['join_date'] ?></td>
+                    <td class="actions">
+                      <a href="update.php?id=<?= $contact['id'] ?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                      <a href="delete.php?id=<?= $contact['id'] ?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                    </td>
+                  </tr>
+                <?php endforeach; ?>
+              </tbody>
+            </table>
+
+
+            <div class="pagination">
+              <?php if ($page_no > 1) : ?>
+                <a href="volunteer?page=<?= $page_no - 1 ?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
+              <?php endif; ?>
+              <?php if ($page_no * $records_per_page < $num_contacts) : ?>
+                <a href="volunteer?page=<?= $page_no + 1 ?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
+              <?php endif; ?>
+            </div>
+          </div>
+
+    </div>
+
+    <div id="tab2C" class="tab-container">
+      <div class="content read">
+
+        <table>
+          <thead>
+            <tr>
+              <td>Requested On</td>
+              <td>Name</td>
+              <td>Joined Date</td>
+              <td></td>
+            </tr>
           </thead>
           <tbody>
 
-            <?php foreach ($contacts as $contact) : ?>
+            <?php foreach ($newReq as $req) : ?>
               <tr>
-                <td><?= $contact['id'] ?></td>
-                <td><?= $contact['vol_id'] ?></td>
-                <td><?= $contact['name'] ?></td>
-                <td><?= $contact['level'] ?></td>
-                <td><?= $contact['vol_points'] ?></td>
-                <td><?= $contact['join_date'] ?></td>
+                 
+                <td><?= $req['join_date'] ?></td>
+                <td><?= $req['vol_id'] ?></td>
+                <td><?= $req['name'] ?></td>
+             
                 <td class="actions">
-                  <a href="update.php?id=<?= $contact['id'] ?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
-                  <a href="delete.php?id=<?= $contact['id'] ?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
+                  <a href="update.php?id=<?= $req['id'] ?>" class="edit"><i class="fas fa-pen fa-xs"></i></a>
+                  <a href="delete.php?id=<?= $req['id'] ?>" class="trash"><i class="fas fa-trash fa-xs"></i></a>
                 </td>
               </tr>
             <?php endforeach; ?>
           </tbody>
         </table>
-        <div class="pagination">
-          <?php if ($page_no > 1) : ?>
-            <a href="volunteer?page=<?= $page_no - 1 ?>"><i class="fas fa-angle-double-left fa-sm"></i></a>
-          <?php endif; ?>
-          <?php if ($page_no * $records_per_page < $num_contacts) : ?>
-            <a href="volunteer?page=<?= $page_no + 1 ?>"><i class="fas fa-angle-double-right fa-sm"></i></a>
-          <?php endif; ?>
-        </div>
-      </div>
 
-      <div id="tab2C" class="tab-container">
-        <h2>Tab2</h2>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Tempore officia officiis ullam nulla nam ipsam, consectetur iusto quos ipsa assumenda dolorum neque molestiae perferendis repudiandae, itaque repellat, labore necessitatibus pariatur rerum odio non hic. Quisquam ipsa quasi nihil accusantium, itaque molestias cupiditate consequatur obcaecati aspernatur ipsam, neque corrupti doloremque molestiae tempora explicabo ea iste.</p>
-      </div>
-
-
-      <div id="tab3C" class="tab-container">
-        <h2>Tab3</h2>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Soluta, sapiente ipsa! Beatae, repellat, excepturi animi tempore possimus quia culpa tempora fuga atque aspernatur quaerat, esse eveniet aperiam eligendi non expedita cumque totam doloremque repellendus pariatur ullam deserunt facilis ipsam sapiente. Tempore, a omnis!</p>
-      </div>
-
+       
+      </div>    
     </div>
 
   </div>
