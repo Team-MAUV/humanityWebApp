357ad5af4973b36afe4ef754c28e921926c2d16e
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2020-11-17 17:45:56 +0530

    header add donation

--- views/homePage/donation_index.php
+++ views/homePage/donation_index.php
@@ -2,42 +2,41 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Humanity</title>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>donation Home </title>
+    <link rel="stylesheet" href="<?= URL ?>public/css/userLogin.css" />
+    <link rel="stylesheet" href="<?= URL ?>public/css/<?php echo $style ?>.css" />
 
-  <link rel="stylesheet" href="<?= URL ?>public/css/index_style.css" />
-  <script src="https://kit.fontawesome.com/c0d2632e91.js" crossorigin="anonymous"></script>
-  <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-  <script src="https://kit.fontawesome.com/a076d05399.js"></script>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,600&display=swap" rel="stylesheet" />
+    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 </head>
 
 <body>
+    <header>
+        <div class="nav-bar">
+            <div class="logo-container">
 
 
+                <div class="logo"></div>
+            </div>
 
-  <div class="nav-bar">
-    <div class="logo-container">
+            <div class="title-name">
 
+                <h3 class="logo-name">
+                    <div class="tzu-logo"></div>
+                    Tzu Chi Foundation | <span> Hambantota</span>
+                </h3>
+            </div>
 
-      <div class="logo"></div>
-    </div>
+            <div class="log-sign" style="--i: 1.8s;">
 
-    <div class="title-name">
+                <a href="<?= URL ?>index" class="back-btn" alt="Sun"><i class="fas fa-home"></i></a>
+            </div>
 
-      <h3 class="logo-name">
-        <div class="tzu-logo"></div>
-        Tzu Chi Foundation | <span> Hambantota</span>
-      </h3>
-    </div>
-    <div class="dropdown">
-      <button class="dropbtn"><i class="fas fa-users-cog"></i>
-        <i class="fa fa-caret-down"></i>
-      </button>
-      <div class="dropdown-content">
-        <a href="<?= URL ?>login">Commissioner</a>
-        <a href="<?= URL ?>login">Staff</a>
+        </div>
 
-      </div>
-    </div>
-  </div>
+    </header>
+
+    
