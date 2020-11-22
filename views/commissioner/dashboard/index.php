8f6c8b08bb5a749b706eae14b9e03a2c7c3658e9
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2020-11-22 22:14:14 +0530

    come home layout fin

--- views/commissioner/dashboard/index.php
+++ views/commissioner/dashboard/index.php
@@ -25,7 +25,21 @@ include "com-dash-header.php"; ?>
   <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
   <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
   
+  <style>
+table {
+  border-collapse: collapse;
+  border-spacing: 0;
+  width: 100%;
+  border: 1px solid #ddd;
+}
 
+th, td {
+  text-align: left;
+  padding: 8px;
+}
+
+tr:nth-child(even){background-color: #f2f2f2}
+</style>
 	
 
 	<!-- End import lib -->
@@ -126,12 +140,98 @@ include "com-dash-header.php"; ?>
     <div class="row">
 			<div class="col-8 col-m-12 col-sm-12">
 				<div class="card">
-          hello
+        <div class="form_wrapper">
+        <div class="form_container">
+    <div class="title_container">
+      <h2>Responsive Registration Form</h2>
+    </div>
+    <div class="row clearfix">
+      <div class="">
+        <form>
+          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-envelope"></i></span>
+            <input type="email" name="email" placeholder="Email" required />
+          </div>
+          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+            <input type="password" name="password" placeholder="Password" required />
+          </div>
+          <div class="input_field"> <span><i aria-hidden="true" class="fa fa-lock"></i></span>
+            <input type="password" name="password" placeholder="Re-type Password" required />
+          </div>
+          <div class="row clearfix">
+            <div class="col_half">
+              <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                <input type="text" name="name" placeholder="First Name" />
+              </div>
+            </div>
+            <div class="col_half">
+              <div class="input_field"> <span><i aria-hidden="true" class="fa fa-user"></i></span>
+                <input type="text" name="name" placeholder="Last Name" required />
+              </div>
+            </div>
+          </div>
+            	<div class="input_field radio_option">
+              <input type="radio" name="radiogroup1" id="rd1">
+              <label for="rd1">Male</label>
+              <input type="radio" name="radiogroup1" id="rd2">
+              <label for="rd2">Female</label>
+              </div>
+              <div class="input_field select_option">
+                <select>
+                  <option>Select a country</option>
+                  <option>Option 1</option>
+                  <option>Option 2</option>
+                </select>
+                <div class="select_arrow"></div>
+              </div>
+            <div class="input_field checkbox_option">
+            	<input type="checkbox" id="cb1">
+    			<label for="cb1">I agree with terms and conditions</label>
+            </div>
+            <div class="input_field checkbox_option">
+            	<input type="checkbox" id="cb2">
+    			<label for="cb2">I want to receive the newsletter</label>
+            </div>
+          <input class="button" type="submit" value="Register" />
+        </form>
+      </div>
+    </div>
+  </div>
+        </div>
 				</div>
       </div>
       <div class="col-4 col-m-12 col-sm-12">
 				<div class="card">
-          hi
+        <div style="overflow-x:auto;">
+  <table>
+    <tr>
+      <th>Commissioner Name</th>
+      <th>Com-ID</th>
+      </tr>
+    <tr>
+    <td>Perera</td>
+      <td>COMHB001</td>
+    </tr>
+    <tr>
+    <td>Smith</td>
+      <td>COMHB002</td>
+    </tr>
+    <tr>
+    <td>Perera</td>
+      <td>COMHB003</td>
+    </tr> <tr>
+    <td>Perera</td>
+      <td>COMHB004</td>
+    </tr>
+    <tr>
+    <td>Perera</td>
+      <td>COMHB005</td>
+    </tr>
+    <tr>
+    <td>Jill</td>
+      <td>COMHB006</td>
+    </tr>
+    </table>
+</div>
         </div>
       </div>
     </div>
