1726293f76f672ea88ee1966987f5e58a7a4fd65
Manuli1998 <manuli1998@gmail.com>
2020-11-30 15:26:01 +0530

    Donor and buyer logout

--- views/buyer/dashboard/buyer_header.php
+++ views/buyer/dashboard/buyer_header.php
@@ -124,8 +124,8 @@
           </h3>
         </div>
         <div class="dropdown">
-          <button class="dropbtn">Hi, <?= $_SESSION['name'] ?>
-            <i class="fa fa-caret-down"></i>
+        <button class="dropbtn">Hi, <?= $_SESSION['name'] ?>
+          <i class="fa fa-caret-down"></i>
           </button>
           <div class="dropdown-content">
             <a href="#">Edit Profile</a>
