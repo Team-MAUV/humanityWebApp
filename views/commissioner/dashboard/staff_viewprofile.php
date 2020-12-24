177a422f1a094ed7988085769e4be8f8adefe7ef
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2020-12-24 10:17:33 +0530

    leave ui cal add

---
+++ views/commissioner/dashboard/staff_viewprofile.php
@@ -0,0 +1,20 @@
+<?php $page = 'staff_viewprofile';
+$title = 'staff_viewprofile';
+$style = 'com_dash_staffviewprofile.css';
+include "com-dash-header.php"; ?>
+
+
+<div class="grid-container">
+
+
+
+  <div id="breadcrum">
+
+  <!DOCTYPE html>
+<html>
+<head>
+	<title>Saff Profile</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+</head>
