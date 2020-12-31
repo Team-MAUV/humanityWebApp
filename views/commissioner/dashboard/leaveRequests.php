52bdf7a36214a4a5001d8183e98e458e78bb3cee
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2020-12-31 21:13:08 +0530

    leve request viewform add

--- views/commissioner/dashboard/leaveRequests.php
+++ views/commissioner/dashboard/leaveRequests.php
@@ -38,6 +38,78 @@ include "com-dash-header.php"; ?>
 </div>
 -->
 
+
+<div id="breadcrum">
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Saff Profile</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+<link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
+</head>
+<style>
+
+* {box-sizing: border-box;}
+
+/* Button used to open the contact form - fixed at the bottom of the page */
+.open-button {
+  background-color: #555;
+  color: white;
+  padding: 16px 20px;
+  border: none;
+  cursor: pointer;
+  opacity: 0.8;
+
+
+
+}
+
+/* The popup form - hidden by default */
+.form-popup {
+  display: none;
+  position: fixed;
+  top:100px;
+right:300px;
+  border: 6px solid blue;
+  z-index: 9;
+  text-align: left;
+  
+}
+
+/* Add styles to the form container */
+.form-container {
+  max-width: 800px;
+  padding: 40px;
+  background-color:#9a9ae5;
+  color:black;
+  text-align: left;
+  
+}
+.form-container .btn {
+  background-color: #4CAF50;
+  color: white;
+  padding: 10px 16px;
+  border: none;
+  cursor: pointer;
+  width: 80%;
+  margin-bottom:10px;
+  opacity: 0.8;
+}
+
+/* Add a red background color to the cancel button */
+.form-container .cancel {
+  background-color: red;
+}
+
+/* Add some hover effects to buttons */
+.form-container .btn:hover, .open-button:hover {
+  opacity: 1;
+}
+</style>
+<body>
+
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <h1>Leave Requests </h1>
 <div id="bttnContainer">
@@ -47,8 +119,8 @@ include "com-dash-header.php"; ?>
 <br>
 <!--row start-->
 <div class="row">
-
- <button type="button" class="readmore">Nimali <span>  &nbsp; &nbsp; &nbsp;</span> 20.01.2021</button>
+<!--one staff-->
+ <button type="button" class="readmore">Nimali <span>&nbsp;&nbsp;</span> STFHB001 <span>  &nbsp; &nbsp; &nbsp;</span> 20.01.2021</button>
   <div class="content">
     <p>
     <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
@@ -60,33 +132,34 @@ include "com-dash-header.php"; ?>
 
         <button type="button" class="btn" name="submit">Submit</button> </div>
   <p style="color:white;font-family:sans-serif">Already taken: 2</p>
-        <button type="button" class="btn" onclick="viewform()">View Request Form</button>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
     </p>
    
    </div>
 
-   <button type="button" class="readmore">Perera 
-   </button>
+<!--second  staff-->
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
   <div class="content">
     <p>
-
- 
-      <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
-      <input type="radio" id="decline" name="confirmreq" value="decline" ><br>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
   
- <input  style="display:none;" type="text" id="reason" placeholder="Reason to decline" name="reason"  >
   
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
 
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
    
-        <button type="button" class="btn" name="submit">Submit</button> </div>
-  <p>Already taken: 2</p>
-        <button type="button" class="btn" onclick="viewform()">View Request Form</button>
-      
-
-    </p>
    </div>
-   <button type="button" class="readmore">Nimali 
-   </button>
+
+
+
+<!--3 staff-->
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
   <div class="content">
     <p>
     <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
@@ -98,14 +171,13 @@ include "com-dash-header.php"; ?>
 
         <button type="button" class="btn" name="submit">Submit</button> </div>
   <p style="color:white;font-family:sans-serif">Already taken: 2</p>
-        <button type="button" class="btn" onclick="viewform()">View Request Form</button>
-      
-
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
     </p>
+   
    </div>
+<!--4  staff-->
 
-   <button type="button" class="readmore">Nimali 
-   </button>
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
   <div class="content">
     <p>
     <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
@@ -117,17 +189,298 @@ include "com-dash-header.php"; ?>
 
         <button type="button" class="btn" name="submit">Submit</button> </div>
   <p style="color:white;font-family:sans-serif">Already taken: 2</p>
-        <button type="button" class="btn" onclick="viewform()">View Request Form</button>
-      
-
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
     </p>
+   
    </div>
 
 
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
+   </div>
+
+
+   <button type="button" class="readmore">Kamal <span> &nbsp;&nbsp;</span> STFHB002  <span>  &nbsp; &nbsp; &nbsp;</span> 28.01.2021</button>
+  <div class="content">
+    <p>
+    <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline 
+      <input type="radio" id="decline" name="confirmreq" value="decline"><br>
+  
+  
+          <input style="display:none;" type="text" id="reason" placeholder="Reason to decline " name="reason">
+     
+
+        <button type="button" class="btn" name="submit">Submit</button> </div>
+  <p style="color:white;font-family:sans-serif">Already taken: 2</p>
+        <button type="open-button" class="btn" onclick="viewform()">View Request Form</button>
+    </p>
+   
    </div>
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   <!--popup form-->
+   <div class="form-popup" id="myForm">
+  <div class="form-container">
+    <h2>Nimali<span>&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;</span>STFHB002</h2>
+<hr style="color:blue"> </hr>
+    <h3>From: 23.01.2021 </h3>
+    <h3>To : 23.01.2021 </h3>
+    <h4>Wedding to my friend </h4>
+
+
+    
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </div>
+</div>
+
+<script>
+function viewform() {
+  document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
+</script>
+
+
+
+
+
+</div>
+   
 
 
 
