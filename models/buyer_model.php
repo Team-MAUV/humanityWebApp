be99fc8ef70ba919851f3900aeb2e6888fb36032
UthpalaK97 <kasunuthpalak@gmail.com>
2021-01-06 12:32:33 +0530

    view_product model updated

--- models/buyer_model.php
+++ models/buyer_model.php
@@ -5,61 +5,31 @@ class buyer_model extends Model{
     parent::__construct();
   }
 
-
-  public function view_glass(){
-    $st = $this->db->prepare('SELECT * FROM product WHERE type="Glass"');  
-    $st->execute();
-    $product = $st->fetchAll();
+  public function view_product(){
     
-    
-    $pagedata = [
-      'product' => $product
-    ];
-    return ($pagedata);
-  }
-  public function view_plastic(){
-    $st = $this->db->prepare('SELECT * FROM product WHERE type="Plastic"');  
+    if($_GET['prd']=='Plastic'){
+      $st = $this->db->prepare('SELECT * FROM product WHERE type="Plastic"');   
+    }else if($_GET['prd']=='Glass'){
+      $st = $this->db->prepare('SELECT * FROM product WHERE type="Glass"');
+    }else if($_GET['prd']=='Paper'){
+      $st = $this->db->prepare('SELECT * FROM product WHERE type="Paper"');
+    }else if($_GET['prd']=='Electronic'){
+      $st = $this->db->prepare('SELECT * FROM product WHERE type="Electronic"');
+    }else if($_GET['prd']=='Other'){
+      $st = $this->db->prepare('SELECT * FROM product WHERE type="Other"');
+    }
     $st->execute();
-    $product = $st->fetchAll();
-   
-    $pagedata = [
-      'product' => $product
-    ];
-    return ($pagedata);
+      $product = $st->fetchAll();
     
-  }
-  public function view_paper(){
-    $st = $this->db->prepare('SELECT * FROM product WHERE type="Paper"');  
-    $st->execute();
-    $product = $st->fetchAll();
-   
       $pagedata = [
         'product' => $product
       ];
       return ($pagedata);
-    }
     
+  }
   
-  public function view_electronic(){
-    $st = $this->db->prepare('SELECT * FROM product WHERE type="Electronic"');  
-    $st->execute();
-    $product = $st->fetchAll();
-    
-    $pagedata = [
-      'product' => $product
-    ];
-    return ($pagedata);
-  }
-  public function view_other(){
-    $st = $this->db->prepare('SELECT * FROM product WHERE type="Other"');  
-    $st->execute();
-    $product = $st->fetchAll();
-    
-    $pagedata = [
-      'product' => $product
-    ];
-    return ($pagedata);
-  }
+  
+  
 
   
 }
