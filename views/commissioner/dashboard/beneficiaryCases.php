70b6c3cdab71c8d39c05a0391e4bedcc18c18672
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-03-18 07:03:27 +0530

    ben error and extra del

--- views/commissioner/dashboard/beneficiaryCases.php
+++ views/commissioner/dashboard/beneficiaryCases.php
@@ -1,91 +1,4 @@
 
-<!--
-<div class="row">
-    <button class="tab" onclick="openpage('upcoming',this)" id="defaultOpen">Upcoming Cases</button>
-    <button class="tab" onclick="openpage('current',this)">Current Cases</button>
-    <button class="tab" onclick="openpage('finished',this)">Finished Cases</button></div>
-
-    <div id="upcoming" class="tabcontent">
-
-    <div class="split left">
-  
-  <h1> Upcoming Beneficary Cases</h1></br>
-  <?php foreach ($upcomings as $upcoming) : ?>
-  <button type="button" class="readmore"><?= $upcoming['beneficiery_id'] ?></button>
-  <div class="content">
-    <p>
-
-
-      <div class="confirm"> Accept <input type="radio" id="accept" name="confirmreq" value="accept"> &nbsp;Decline <input type="radio" id="decline" name="confirmreq" value="decline"><br>
-        <button type="button" class="btn" name="submit">Submit</button> </div>
-      <button class="dbtn"><a href="<?= $upcoming['case_path'] ?>" download="<?= $upcoming['beneficiery_id'] ?>"><i class="fa fa-download"></i> Download</a></button>
-
-    </p>
-  </div>
-  
-  
-  <?php endforeach; ?>
-  
-</div>
-
-    </div>
-    <div id="current" class="tabcontent">
-
-    <div class="split left">
-  
-  <h1>Ongoing Beneficary Cases</h1></br>
-  <?php foreach ($currents as $current) : ?>
-  <button type="button" class="readmore"><?= $current['beneficiery_id'] ?></button>
-  <div class="content">
-    <p>
-
-    <?php if(strlen($current['vol_id'])==1 && strlen($current['vol_id'])>0){
-      $ccustomid ="VOLHB00".$current['vol_id'];
-    }else if(strlen($current['vol_id'])==2 && strlen($current['vol_id'])>0){
-      $ccustomid ="VOLHB0".$current['vol_id'];
-    }else if(strlen($current['vol_id'])>0){
-      $ccustomid ="VOLHB".$current['vol_id'];
-    };
-    ?>
-    <div class="subby">Submitted By:</br><?= $ccustomid ?></div>
-      
-      <button class="dbtn"><a href="<?= $current['case_path'] ?>" download="<?= $current['beneficiery_id'] ?>"><i class="fa fa-download"></i> Download</a></button>
-
-    </p>
-  </div>
-  <?php endforeach; ?>
-  
-</div>
-    </div>
-    <div id="finished" class="tabcontent">
-    <div class="split left">
-  
-  <h1>Closed Beneficary Cases</h1></br>
-  <?php foreach ($finishs as $finish) : ?>
-  <button type="button" class="readmore"><?= $finish['beneficiery_id'] ?></button>
-  <div class="content">
-    <p>
-
-    <?php if(strlen($finish['vol_id'])==1 && strlen($finish['vol_id'])>0){
-      $customid ="VOLHB00".$finish['vol_id'];
-    }else if(strlen($finish['vol_id'])==2 && strlen($finish['vol_id'])>0){
-      $customid ="VOLHB0".$finish['vol_id'];
-    }else if(strlen($finish['vol_id'])>0){
-      $customid ="VOLHB".$finish['vol_id'];
-    };
-    ?>
-    <div class="subby">Submitted By:</br><?= $customid ?></div>
-      <button class="dbtn"><a href="<?= $finish['case_path'] ?>" download="<?= $finish['beneficiery_id'] ?>"><i class="fa fa-download"></i> Download</a></button>
-
-    </p>
-  </div>
-  <?php endforeach; ?>
-  
-</div>
-        
-    </div>
-  -->
-
 
 
   <?php $page = 'beneficiaryCases';
@@ -106,11 +19,9 @@ include "com-dash-header.php"; ?>
 <html>
 <head>
 <title>volunteer Profile</title>
-<link rel="stylesheet" type="text/css" href="style.css">
 <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
 <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0">
 
-<link rel="stylesheet" href="<?= URL?> public/css/com_dash_readmore.css" />
 </head>
 <body>
 
@@ -123,8 +34,8 @@ include "com-dash-header.php"; ?>
 <div class="tab-section">
   <ul>
     <li><a id="tab1" href="?upcoming" class="active">Upcoming Cases </a></li>
-    <li><a id="tab2" href="?current"> Current Cases </a></li>
-    <li><a id="tab3" href="?finish"> Finished Cases</a></li>
+    <li><a id="tab2" href="#"> Current Cases </a></li>
+    <li><a id="tab3" href="#"> Finished Cases</a></li>
       
     
   </ul>
