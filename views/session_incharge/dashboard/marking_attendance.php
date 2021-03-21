118f4cbdfe63f7ecabeb5ff8a21d8323615347cf
Vigani Ravikkumar <61198424+VigaRavi@users.noreply.github.com>
2021-03-21 06:39:45 +0530

    css js file changed

--- views/session_incharge/dashboard/marking_attendance.php
+++ views/session_incharge/dashboard/marking_attendance.php
@@ -1,6 +1,6 @@
 <?php $page = 'marking_attendance';
-include "sess_header.php"; ?>
-
+include "sess_header.php"; 
+$style = 'sess_dash_markattendance.css';?>
 
 <div class="grid-container">
 
@@ -18,80 +18,20 @@ include "sess_header.php"; ?>
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+
     <title>Table</title>
+
+    <link rel="stylesheet" href="<?= URL?> public/css/sess_dash_markattendance.css" />
 </head>
 
-<style>
-    body {
-        padding: 0px;
-        margin: 0;
-        font-family: Verdana, Geneva, Tahoma, sans-serif;
-    }
-    
-    table {
-        position: absolute;
-        left: 50%;
-        top: 50%;
-        transform: translate(-50%, -50%);
-        border-collapse: collapse;
-        width: 800px;
-        height: 200px;
-        border: 1px solid #bdc3c7;
-        box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);
-    }
-    
-    tr {
-        transition: all .2s ease-in;
-        cursor: pointer;
-    }
-    
-    th,
-    td {
-        padding: 12px;
-        text-align: left;
-        border-bottom: 1px solid #ddd;
-    }
-    
-    #header {
-        background-color:#008B8B;
-        color: #fff;
-    }
-    
-    h1 {
-        font-weight: 600;
-        text-align: center;
-        background-color:#008B8B;
-        color: #fff;
-        padding: 10px 0px;
-    }
-    
-    tr:hover {
-        background-color: #f5f5f5;
-        transform: scale(1.02);
-        box-shadow: 2px 2px 12px rgba(0, 0, 0, 0.2), -1px -1px 8px rgba(0, 0, 0, 0.2);
-    }
-    .checkbox{
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 25px;
-  width: 25px;
-  background-color:gray;
-}
-
-    @media only screen and (max-width: 768px) {
-        table {
-            width: 90%;
-        }
-    }
-</style>
 
 <body>
 
     <h1>Confirmed Participants attendance marking</h1>
     <hr>
 
-    <table>
+    <table id="att">
         <tr id="header">
             <th>Vol_id</th>
             <th>Volunteer  name</th>
