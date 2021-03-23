91df41ca717628ffb6182b7ccec51a80ac39be25
anjana-dodampe <ahmdodampe@hotmail.com>
2021-03-23 12:49:18 +0530

    db export

--- db dump file/23-03-2021 - Anjana/humanity.sql
+++ db dump file/23-03-2021 - Anjana/humanity.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 23, 2021 at 08:14 AM
+-- Generation Time: Mar 23, 2021 at 08:18 AM
 -- Server version: 10.4.13-MariaDB
 -- PHP Version: 7.4.7
 
