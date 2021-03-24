e3b7d9ecbf00f1f46432685cf65ee6bb790f97de
anjana-dodampe <ahmdodampe@hotmail.com>
2021-03-24 13:53:52 +0530

    remote host changedd

--- config/database.php
+++ config/database.php
@@ -1,6 +1,6 @@
 <?php
 
-define('DB_TYPE', 'mysql');
+define('DB_TYPE', 'remotemysql');
 define('DB_HOST', 'remotemysql.com:3306');
 define('DB_NAME', 'QfK5Zr7gtz');
 define('DB_USER', 'QfK5Zr7gtz');
