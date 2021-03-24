9d3c515020374d53a1a013615048257fa90ad456
anjana-dodampe <ahmdodampe@hotmail.com>
2021-03-24 13:58:00 +0530

    minor change

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
-define('DB_TYPE', 'remotemysql');
-define('DB_HOST', 'remotemysql.com:3306');
+define('DB_TYPE', 'mysql');
+define('DB_HOST', 'remotemysql.com');
 define('DB_NAME', 'QfK5Zr7gtz');
 define('DB_USER', 'QfK5Zr7gtz');
 define('DB_PASS', 'KcE2A5Oe5b');
