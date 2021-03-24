989f005994be879e11cbeba8abbb89ccaf3b19f8
anjana-dodampe <ahmdodampe@hotmail.com>
2021-03-24 23:41:53 +0530

    new

--- config/database.php
+++ config/database.php
@@ -1,7 +1,7 @@
 <?php
 
 define('DB_TYPE', 'mysql');
-define('DB_HOST', '204.2.195.80');
+define('DB_HOST', 'https://mysql-24781-0.cloudclusters.net');
 define('DB_NAME', 'humanity');
 define('DB_USER', 'tzuchihumanity');
 define('DB_PASS', 'humanity123');
