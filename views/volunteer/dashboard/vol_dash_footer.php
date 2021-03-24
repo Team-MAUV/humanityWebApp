d607794e8a382f49e69b5b4078061eb5ed490915
Manuli1998 <manuli1998@gmail.com>
2021-03-24 16:07:09 +0530

    Volunteer all pages indentation

--- views/volunteer/dashboard/vol_dash_footer.php
+++ views/volunteer/dashboard/vol_dash_footer.php
@@ -1,11 +1,7 @@
 <footer>
-  <div class="footer">Powered by<h4>Humanity2020&copy;</div>
+      <div class="footer">Powered by<h4>Humanity2020&copy;</div>
 </footer>
-
 </div>
-
-
-
 </body>
 
 <script src="https://kit.fontawesome.com/c0d2632e91.js" crossorigin="anonymous"></script>
@@ -13,38 +9,34 @@
 <script src="https://kit.fontawesome.com/a076d05399.js"></script>
 
 <script>
-  $(".feat-btn").click(function() {
-    $("div ul .feat-show").toggleClass("show");
-    $("div ul .first").toggleClass("rotate");
-  });
-  $(".serv-btn").click(function() {
-    $("div ul .serv-show").toggleClass("show1");
-    $("div ul .second").toggleClass("rotate");
-  });
-  $("div ul li").click(function() {
-    $(this).addClass("active").siblings().removeClass("active");
-  });
+      $(".feat-btn").click(function() {
+          $("div ul .feat-show").toggleClass("show");
+          $("div ul .first").toggleClass("rotate");
+      });
+      $(".serv-btn").click(function() {
+          $("div ul .serv-show").toggleClass("show1");
+          $("div ul .second").toggleClass("rotate");
+      });
+      $("div ul li").click(function() {
+          $(this).addClass("active").siblings().removeClass("active");
+      });
 </script>
-
 
 <script>
-  /// Display container which has active class
-  var id = $('.tab-section .active').attr('id');
-  $('.tab-container').hide();
-  $('.tab-container#' + id + 'C').show();
-
-  //// Change the tab when user clicks
-  $('.tab-section a').click(function() {
-    // console.log(this.id);
-    if (!$(this).hasClass('active')) {
-      // $('.container').hide();
-
-      $('.tab-section a').removeClass('active');
-      $(this).addClass('active');
+      /// Display container which has active class
+      var id = $('.tab-section .active').attr('id');
       $('.tab-container').hide();
-      $('.tab-container#' + this.id + 'C').show();
-    }
-  });
+      $('.tab-container#' + id + 'C').show();
+      //// Change the tab when user clicks
+      $('.tab-section a').click(function() {
+          // console.log(this.id);
+            if (!$(this).hasClass('active')) {
+                  // $('.container').hide();
+                  $('.tab-section a').removeClass('active');
+                  $(this).addClass('active');
+                  $('.tab-container').hide();
+                  $('.tab-container#' + this.id + 'C').show();
+            }
+      });
 </script>
-
 </html>
