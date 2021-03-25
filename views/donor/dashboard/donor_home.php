418f4a6105bca265bc42cb458e991938ad0a18ec
anjana-dodampe <ahmdodampe@hotmail.com>
2021-03-25 19:32:30 +0530

    Donor amount btn js finished

--- views/donor/dashboard/donor_home.php
+++ views/donor/dashboard/donor_home.php
@@ -11,26 +11,35 @@ include "don_dash_header.php"; ?>
                        <div class="view1" id="donateView" style="display: block;">
                             <h4>Select an amount</h4>
                             <ul class="select-value">
-                                <li><button type="button" class="don-value">Rs:50.00</button> </li>
-                                <li><button type="button" class="don-value">Rs:250.00</button> </li>
-                                <li><button type="button" class="don-value">Rs:500.00</button> </li>
-                                <li><button type="button" class="don-value">Rs:1000.00</button> </li>
-                                <li><button type="button" class="don-value">Rs:5000.00</button> </li>
-                                <li><button type="button" class="don-value">Other</button> </li>
-                                
+                                <li><button type="button" id="btn1" class="don-value" onclick="insert50()">Rs:50.00</button> </li>
+                                <li><button type="button" id="btn2" class="don-value" onclick="insert100()">Rs:100.00</button> </li>
+                                <li><button type="button" id="btn3" class="don-value" onclick="insert500()">Rs:500.00</button> </li>
+                                <li><button type="button" id="btn4" class="don-value" onclick="insert1000()">Rs:1000.00</button> </li>
+                                <li><button type="button" id="btn5"class="don-value" onclick="insert5000()">Rs:5000.00</button> </li>
+                                <button onclick="showOther();" id="otherBtn" class="don-value">Other</button>
+
                             </ul>
-                            <div class="cust-amount">
+                            <div style="display:none" class="cust-amount" id="cust-amount">
                                 <label for="amount">Donate other amount (Rs) : </label>
-                                <input class="amount" type="text">         
+                                <input type="text" id="inputAmount" name="amount"class="amount" value="" >   
+                                <button type="button" id="btn5"class="don-value" onclick="getInputVal()">Add Amount</button>
+                                <br>
+                                       
                             </div>
+
+                            <div class="cust-amount"> <label for="amount">Donating Amount (Rs) : </label>
+                                <input class="amount" type="text" id="textField" value="" required></input> 
+                            </div>
+
                             <div class="donate-for">
                                 <label for="use-donation">Use my donation for :</label>
                                 <select name="select-donation" id="select-donation">
-                                    <option value="">Charity-Provide continous hepl tp those in need</option>
-                                    <option value="">Desaster-</option>
-                                    <option value="">Development-</option>
+                                    <option value="">Charity-Provide continous help to those in need</option>
+                                    <option value="">Disaster</option>
+                                    <option value="">Development</option>
                                 </select>
                             </div>
+
                             <div class="dedicate-donation">
                                 <input type="checkbox" id="dedicate" onclick="showDedicateDonation()">
                                 <label for="dedicate"> Dedicate this to a loved one</label> 
@@ -62,7 +71,7 @@ include "don_dash_header.php"; ?>
                                 <input type="hidden" name="address" value="No.1, Galle Road">
                                 <input type="hidden" name="city" value="Colombo">
                                 <input type="hidden" name="country" value="Sri Lanka"><br><br> 
-                                <input class="paybtn" type="submit" value="Pay Now">   
+                                <input  class="don-value" type="submit" value="Pay Now">   
 
                             </form> 
 
@@ -77,34 +86,38 @@ include "don_dash_header.php"; ?>
                             
                             
                         </div>
-                        <div class="view2" id="viewDonations" style="display: none;">
-                            <span onclick="closeMyDonations()" class="close-btn">&times;</span>
-                            <div class="stats">
-                                <div class="my-donations">
-                                    <h2>Donor ID : <?=$_SESSION['id']?></h2>
-                                    <ul>
+                            <div class="view2" id="viewDonations" style="display: none;">
+                                 <span onclick="closeMyDonations()" class="close-btn">&times;</span>
+                                 <div class="stats">
+                                     <div class="my-donations">
+                                        <h2>Donor ID : <?=$_SESSION['id']?></h2>
+                                            <ul>
                                     
-                                    <?php foreach ($donation as $donations) : ?>
+                                                <?php foreach ($donation as $donations) : ?>
                                     
-                                        <li>Your total Contribution(Rs) : <?=$donations['contribution']?></li>
-                                        <?php endforeach; ?>
-                                        <?php foreach ($maxcon as $maxcons) : ?>
-                                        <li>Largest Contribution(Rs) : <?=$maxcons['maxim']?></li>
-                                        <?php endforeach; ?>
-                                        <?php foreach ($latestcon as $latestcons) : ?>
-                                        <li>Latest Contribution on : <?=$latestcons['maximd']?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
+                                                    <li>Your total Contribution(Rs) : <?=$donations['contribution']?></li>
+                                                 <?php endforeach; ?>
+
+                                                 <?php foreach ($maxcon as $maxcons) : ?>
+                                                      <li>Largest Contribution(Rs) : <?=$maxcons['maxim']?></li>
+                                                 <?php endforeach; ?>
+
+                                                 <?php foreach ($latestcon as $latestcons) : ?>
+
+                                                      <li>Latest Contribution on : <?=$latestcons['maximd']?></li>
+                                                 <?php endforeach; ?>
+                                            </ul>
                             
-                                </div>
-                                <div class="don-stats">
-                                    <h2>Humanty Donation Stat</h2>
-                                    <ul>
-                                    <?php foreach ($donnum as $donnums) : ?>
-                                        <li>Total number of Donors : <?=$donnums['donc']?></li>
-                                        <?php endforeach; ?>
-                                    </ul>
-                                </div>
+                                     </div>
+
+                                     <div class="don-stats">
+                                         <h2>Humanty Donation Stat</h2>
+                                         <ul>
+                                                <?php foreach ($donnum as $donnums) : ?>
+                                                      <li>Total number of Donors : <?=$donnums['donc']?></li>
+                                                <?php endforeach; ?>
+                                         </ul>
+                                     </div>
                             </div>
                             <div class="latest-donations">
                                 <h2>Your Latest Donations </h2>
@@ -129,4 +142,68 @@ include "don_dash_header.php"; ?>
             
 
 
+<script>
+                function insert50(){
+                    document.getElementById("textField").value =50.00;
+                    document.getElementById('cust-amount').style.display='none'
+                }
 
+                function insert100(){
+                    document.getElementById("textField").value =100.00;
+                    document.getElementById('cust-amount').style.display='none'
+                }
+
+                function insert500(){
+                    document.getElementById("textField").value =500.00;
+                    document.getElementById('cust-amount').style.display='none'
+                }
+
+                function insert1000(){
+                    document.getElementById("textField").value =1000.00;
+                    document.getElementById('cust-amount').style.display='none'
+                }
+
+                function insert5000(){
+                    document.getElementById("textField").value =5000.00;
+                    document.getElementById('cust-amount').style.display='none'
+                }
+
+                function insert5000(){
+                    document.getElementById("textField").value =5000.00;
+                    document.getElementById('cust-amount').style.display='none'
+                }
+
+
+                function getInputVal(){
+                    var inputVal = document.getElementById('inputAmount').value;
+                    document.getElementById("textField").value =inputVal;
+
+                }
+
+
+
+                function showOther() {
+                                // get the Div
+                        var getDiv = document.getElementById('cust-amount');
+
+                                // get the current value of the div's display property
+                        var displaySetting = getDiv.style.display;
+
+                                //get the  button, so we can change what it says
+                            var otherBtn = document.getElementById('otherBtn');
+
+                    // now toggle the clock and the button text, depending on current state
+                            if (displaySetting == 'block') {
+                                    // clock is visible. hide it
+                                    getDiv.style.display = 'none';
+                                    // change button text
+                                    otherBtn.innerHTML = 'Show Other';
+                            }
+                            else {
+                                        // clock is hidden. show it
+                                        getDiv.style.display = 'block';
+                                        // change button text
+                                        otherBtn.innerHTML = 'Hide Other';
+                            }
+                    }
+</script>
