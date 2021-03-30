b221265168da4e5ed31a85f795a93276ce1919c2
anjana-dodampe <ahmdodampe@hotmail.com>
2021-03-31 00:06:54 +0530

    Report Generation Title done

--- libs/Pdf.php
+++ libs/Pdf.php
@@ -1,79 +1,35 @@
 <?php
+ 
+class PDF 
+{
+  public static function report() {
 
-class Pdf {
-
-  public static function pdf_gen() {
-
-
-  /* Include the main TCPDF library (search for installation path). */
-require_once('tcpdf/tcpdf_autoconfig.php');
-
-/* create new PDF document */
-$pdf = new TCPDF(PDF_PAGE_ORIENTATION, PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-
-/* set document information */
-$pdf->SetCreator(PDF_CREATOR);
-$pdf->SetAuthor('Nicola Asuni');
-$pdf->SetTitle('TCPDF Example 001');
-$pdf->SetSubject('TCPDF Tutorial');
-$pdf->SetKeywords('TCPDF, PDF, example, test, guide');
+    ob_start();
+  require('fpdf182/fpdf.php');
 
 
-/* set header and footer fonts */
-$pdf->setHeaderFont(Array(PDF_FONT_NAME_MAIN, '', PDF_FONT_SIZE_MAIN));
-$pdf->setFooterFont(Array(PDF_FONT_NAME_DATA, '', PDF_FONT_SIZE_DATA));
-
-/* set default monospaced font */
-$pdf->SetDefaultMonospacedFont(PDF_FONT_MONOSPACED);
-
-/* set margins */
-$pdf->SetMargins(PDF_MARGIN_LEFT, PDF_MARGIN_TOP, PDF_MARGIN_RIGHT);
-$pdf->SetHeaderMargin(PDF_MARGIN_HEADER);
-$pdf->SetFooterMargin(PDF_MARGIN_FOOTER);
-
-/* set auto page breaks
- $pdf->SetAutoPageBreak(TRUE, PDF_MARGIN_BOTTOM);*/
-
-/* set image scale factor 
- $pdf->setImageScale(PDF_IMAGE_SCALE_RATIO); */
-
-/* set some language-dependent strings (optional) */
-if (@file_exists(dirname(__FILE__).'/lang/eng.php')) {
-	require_once(dirname(__FILE__).'/lang/eng.php');
-	$pdf->setLanguageArray($l);
-}
-
-/* --------------------------------------------------------- */
-
-/* set default font subsetting mode */
-$pdf->setFontSubsetting(true);
-
-/* Set font
- dejavusans is a UTF-8 Unicode font, if you only need to
- print standard ASCII chars, you can use core fonts like
- helvetica or times to reduce file size. */
-$pdf->SetFont('dejavusans', '', 14, '', true);
-
-/* Add a page */
-/* This method has several options, check the source code documentation for more information. */
-$pdf->AddPage();
-
-/* set text shadow effect */
-$pdf->setTextShadow(array('enabled'=>true, 'depth_w'=>0.2, 'depth_h'=>0.2, 'color'=>array(196,196,196),
- 'opacity'=>1, 'blend_mode'=>'Normal'));
-
-/* Set some content to print */
-$html = 
-'<h1 align="center">Hello World</h1>';
-
-/* Print text using writeHTMLCell() */
-$pdf->writeHTMLCell(0, 0, '', '', $html, 0, 1, 0, true, '', true);
-
-/* --------------------------------------------------------- */
-
-/* Close and output PDF document
- This method has several options, check the source code documentation for more information. */
-$pdf->Output('example_001.pdf', 'I');
   
- }
+
+
+
+  $pdf = new FPDF();
+  $pdf->AddPage();
+
+  $pdf->Image('https://lh3.googleusercontent.com/NCUT.tzuchin/RwifoWIXw2I/AAAAAAAAAWs/y3eQsZvVHTQ/s400/%E6%85%88%E9%9D%92logo.jpg',60,30,90,0,'JPEG');
+
+
+  $pdf->SetFont('Arial','B',16);
+  $pdf->Cell(190,20,'Humanity Token of Gratitude!',0,0,'C');
+
+
+ 
+  $pdf->Output();
+
+  
+
+
+
+  
+  ob_end_flush(); 
+  }
 }
